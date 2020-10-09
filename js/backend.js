6a3f578207d58b61893bdfd08c4c6bb25a4fe341
Denis Gorshkov <satrasmatrassatras@gmail.com>
2020-10-10 00:12:02 +0300

    Реализовывает модуль backend

---
+++ js/backend.js
@@ -0,0 +1,71 @@
+'use strict';
+
+(() => {
+  const URL = `https://21.javascript.pages.academy/code-and-magick`;
+  const GET = `GET`;
+  const POST = `POST`;
+
+  const createXHR = (method, url, onLoad, onError) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.responseType = `json`;
+    xhr.timeout = 10000;
+
+    xhr.open(method, url);
+
+    xhr.addEventListener(`load`, function () {
+      let error = ``;
+
+      switch (xhr.status) {
+        case 200:
+          onLoad(xhr.response);
+          break;
+
+        case 400:
+          error = `Неверный запрос`;
+          break;
+
+        case 401:
+          error = `Пользователь не авторизован`;
+          break;
+
+        case 404:
+          error = `Ничего не найдено`;
+          break;
+
+        default:
+          error = `Статус ответа: ${xhr.status} ${xhr.statusText}`;
+          break;
+      }
+
+      if (error) {
+        onError(error);
+      }
+    });
+
+    xhr.addEventListener(`error`, function () {
+      onError(`Произошла ошибка соединения`);
+    });
+
+    xhr.addEventListener(`timeout`, function () {
+      onError(`Запрос не успел выполниться за ` + xhr.timeout + `мс`);
+    });
+
+    return xhr;
+  };
+
+  const load = (onLoad, onError) => {
+    const xhr = createXHR(GET, `${URL}/data`, onLoad, onError);
+    xhr.send();
+  };
+
+  const save = (data, onLoad, onError) => {
+    const xhr = createXHR(POST, `${URL}`, onLoad, onError);
+    xhr.send(data);
+  };
+
+  window.backend = {
+    load,
+    save
+  };
+})();
